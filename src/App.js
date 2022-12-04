a9924c8d68c6e9c3f8a7e81370c48c87aa562d83
Volodymyr <volodyaruzhak@gmail.com>
2022-12-04 18:03:14 +0200

    create img for chevysonic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import lighbox1 from './assets/gallery/lightbox1.jpeg';
+import lighbox2 from './assets/gallery/lightbox2.jpeg';
+import lighbox3 from './assets/gallery/lightbox3.jpeg';
+import lighbox4 from './assets/gallery/lightbox4.jpeg';
+import lighbox5 from './assets/gallery/lightbox5.jpeg';
+import lighbox6 from './assets/gallery/lightbox6.jpeg';
+import lighbox7 from './assets/gallery/lightbox7.jpeg';
+import lighbox8 from './assets/gallery/lightbox8.jpeg';
+import lighbox9 from './assets/gallery/lightbox9.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={lighbox1} alt="logo" />
+        <img src={lighbox2} alt="logo" />
+        <img src={lighbox3} alt="logo" />
+        <img src={lighbox4} alt="logo" />
+        <img src={lighbox5} alt="logo" />
+        <img src={lighbox6} alt="logo" />
+        <img src={lighbox7} alt="logo" />
+        <img src={lighbox8} alt="logo" />
+        <img src={lighbox9} alt="logo" />
       </header>
     </div>
   );
